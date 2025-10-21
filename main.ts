59597361c24ac4111577ce94ad32294e024e6d85
Matthew Ruzzi <68619790+matthewruzzi@users.noreply.github.com>
2025-10-21 01:00:43 +0000

    Initial version

--- main.ts
+++ main.ts
@@ -1,8 +1,45 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { parseArgs } from "@std/cli/parse-args";
+import Sitemapper from "sitemapper";
+import { Feed } from "feed";
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  const args = parseArgs(Deno.args, {
+    string: ["sitemap", "title", "site", "output"],
+    alias: { "output": "o" },
+  });
+
+  const sitemapURL = args.sitemap ?? "https://sitemaps.org/sitemap.xml";
+  const title = args.title ?? "Sitemaps";
+  const site = args.site ?? "https://sitemaps.org/";
+  const outputFileName = args.output;
+
+  const Sitemap = new Sitemapper({
+    url: sitemapURL,
+    timeout: 15000, // 15 seconds
+    concurrency: 10,
+    fields: { loc: true, lastmod: true },
+  });
+
+  const sitemap = await Sitemap.fetch();
+  const sites = sitemap.sites;
+
+  const feed = new Feed({
+    title: title,
+    id: site,
+    copyright: "",
+  });
+
+  sites.forEach((page) => {
+    feed.addItem({
+      title: page.loc,
+      link: page.loc,
+      date: new Date(page.lastmod) ?? new Date(2000, 0, 1),
+    });
+  });
+
+  console.log(feed.rss2());
+
+  if (outputFileName) {
+    await Deno.writeTextFile(outputFileName, feed.rss2());
+  }
 }
